db8055aa2fd264fd30b55a7a2f0a3398f8db9b7c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-03 22:16:32 -0400

    flesh out internals a bit

--- main.lua
+++ main.lua
@@ -25,21 +25,59 @@ local new_queue = ecs:new()
 current_queue:add_components({"pos_x", "pos_y"})
 new_queue:add_components({"pos_x", "pos_y"})
 
--- 2d to 1d calcultion
+-- ecs bolt on
+function current_queue:get_entity(index)
+    return ({self.pos_x[index], self.pos_y[index]})
+end
+
+-- user input
+function love.mousepressed( x, y, button, istouch, presses )
+    -- and so begins the flood fill, entry point to flood fill
+    if button == 1 and x <= 800 and x >= 0 and y <= 800 and y >= 0 and current_queue.entity_count == 0 then
+        current_queue:add_entity({
+            pos_x = x,
+            pos_y = y,
+        })
+        print("started")
+    end
+end
+
+
+-- 1d to 2d calcultion
 local function convert_1d_to_2d(i)
     i = i - 1
     local x = (i % 800) % 800
     local y = math.floor(i / 800)
     return({ x, y })
 end
+-- 2d to 1d calculation
+local function convert_2d_to_1d(x,y)
+    return (x * 800) + y
+end
 
+-- engine load entry point
 function love.load()
     math.randomseed(os.time())
     love.graphics.setDefaultFilter("nearest", "nearest")
 end
 
-function love.update()
 
+-- processes the queues and lines up next engine cycles queue
+local function process_queue()
+
+    -- this allows ultimate flexibility for processing of ecs
+    while current_queue.entity_count > 0 do
+        
+    end
+
+    while new_queue.entity_count > 0 do
+
+    end
+end
+
+-- engine update loop
+function love.update()
+    process_queue()
 end
 
 local pass = 0
@@ -48,10 +86,13 @@ local pass = 0
 function love.draw()
 
     for i = 1,640000 do
-        local x,y = unpack(convert_1d_to_2d(i))
-        graphics.setColor(math_random(), math_random(), math_random(), 1)
-        graphics.points( x, y)
+        local new_color = map[i]
+        if new_color == 1 then
+            local x,y = unpack(convert_1d_to_2d(i))
+            graphics.setColor(new_color, 0, 0, 1)
+            graphics.points( x, y)
+        end
     end
 
     pass = pass + 1
-end-- opengl draw loop to engine api
+end
