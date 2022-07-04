4f934755ed8f71a0abd4ab361dc09efc7cba1322
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-03 22:04:43 -0400

    create queues

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require("ecs")
+
 local
 table_insert, math_random, graphics
 =
@@ -5,10 +7,23 @@ table.insert, math.random, love.graphics
 
 local map = {}
 
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
 -- 800 by 800 pixel grid
 for i = 1,640000 do
     table_insert(map,0)
 end
+
+-- current and new queue
+local current_queue = ecs:new()
+local new_queue = ecs:new()
+
+current_queue:add_components({"pos_x", "pos_y"})
+new_queue:add_components({"pos_x", "pos_y"})
 
 -- 2d to 1d calcultion
 local function convert_1d_to_2d(i)
