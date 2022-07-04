41d2ac51a047d293e74f16ed54718f2cfda68a31
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-03 23:32:49 -0400

    bodge together the rest of the project

--- conf.lua
+++ conf.lua
@@ -2,4 +2,5 @@ function love.conf(t)
     t.title = 'Pain Flood Fill aka "pain ting"'
     t.window.height = 800
     t.window.width = 800
+    t.window.vsync = 0
 end
--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
-require("ecs")
-
 local
-table_insert, math_random, graphics
+math_random, graphics,      math_floor
 =
-table.insert, math.random, love.graphics
+math.random, love.graphics, math.floor
 
 local map = {}
 
@@ -13,31 +11,26 @@ function love.keypressed(key)
     end
 end
 
+
+
 -- 800 by 800 pixel grid
-for i = 1,640000 do
-    table_insert(map,0)
+for i = 1,800*800 do
+    table.insert(map,0)
 end
 
 -- current and new queue
-local current_queue = ecs:new()
-local new_queue = ecs:new()
-
-current_queue:add_components({"pos_x", "pos_y"})
-new_queue:add_components({"pos_x", "pos_y"})
-
--- ecs bolt on
-function current_queue:get_entity(index)
-    return ({self.pos_x[index], self.pos_y[index]})
-end
+local current_queue = {}
 
 -- user input
-function love.mousepressed( x, y, button, istouch, presses )
+local started = false
+function love.mousepressed( x, y, button )
     -- and so begins the flood fill, entry point to flood fill
-    if button == 1 and x <= 800 and x >= 0 and y <= 800 and y >= 0 and current_queue.entity_count == 0 then
-        current_queue:add_entity({
+    if button == 1 and x <= 800 and x >= 0 and y <= 800 and y >= 0 and not started then
+        current_queue[1] = {
             pos_x = x,
             pos_y = y,
-        })
+        }
+        started = true
         print("started")
     end
 end
@@ -46,13 +39,11 @@ end
 -- 1d to 2d calcultion
 local function convert_1d_to_2d(i)
     i = i - 1
-    local x = (i % 800) % 800
-    local y = math.floor(i / 800)
-    return({ x, y })
+    return({ (i % 800) % 800, math_floor(i / 800) })
 end
 -- 2d to 1d calculation
 local function convert_2d_to_1d(x,y)
-    return (x * 800) + y
+    return (y * 800) + x
 end
 
 -- engine load entry point
@@ -61,17 +52,70 @@ function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 end
 
+-- basic collision detection
+local function clear(x,y)
+    if x >= 0 and x <= 800 and y >= 0 and y <= 800 and map[convert_2d_to_1d(x,y)] == 0 then
+        return true
+    end
+    return false
+end
+
+-- basic slotting function
+local function free_slot()
+    for i = 1,800000000 do
+        if not current_queue[i] then
+            return i
+        end
+    end
+end
 
 -- processes the queues and lines up next engine cycles queue
 local function process_queue()
 
-    -- this allows ultimate flexibility for processing of ecs
-    while current_queue.entity_count > 0 do
+    local current_loop = 0
+    for index, value in pairs(current_queue) do
+
+        local x = value.pos_x
+        local y = value.pos_y
+
+        map[convert_2d_to_1d(x,y)] = math_random()
+
+        current_queue[index] = nil
+
         
-    end
+        if clear(x + 1 , y) then
+            current_queue[free_slot()] = {
+                pos_x = x + 1,
+                pos_y = y
+            }
+        end
 
-    while new_queue.entity_count > 0 do
+        if clear(x - 1 , y) then
+            current_queue[free_slot()] = {
+                pos_x = x - 1,
+                pos_y = y
+            }
+        end
 
+        
+        if clear(x , y + 1) then
+            current_queue[free_slot()] = {
+                pos_x = x,
+                pos_y = y + 1
+            }
+        end
+
+        if clear(x , y - 1) then
+            current_queue[free_slot()] = {
+                pos_x = x,
+                pos_y = y - 1
+            }
+        end
+        
+        current_loop = current_loop + 1
+        if current_loop >= 133 then
+            break
+        end
     end
 end
 
@@ -80,19 +124,26 @@ function love.update()
     process_queue()
 end
 
-local pass = 0
-
 -- opengl draw loop to engine api
 function love.draw()
 
-    for i = 1,640000 do
-        local new_color = map[i]
-        if new_color == 1 then
-            local x,y = unpack(convert_1d_to_2d(i))
-            graphics.setColor(new_color, 0, 0, 1)
+    for index,value in ipairs(map) do
+        if value ~= 0 then
+            local x,y = unpack(convert_1d_to_2d(index))
+            graphics.setColor(value, 0, 0, 1)
             graphics.points( x, y)
         end
     end
 
-    pass = pass + 1
+    love.graphics.setColor(0,0,0,1)
+    love.graphics.print("PAIN",360,360,0,3,3)
+
+    love.graphics.print("PAIN",0,0,0,3,3)
+
+    love.graphics.print("PAIN",710,750,0,3,3)
+
+    love.graphics.print("PAIN",710,0,0,3,3)
+
+    love.graphics.print("PAIN",0,750,0,3,3)
+
 end
